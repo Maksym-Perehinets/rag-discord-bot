4d2197a307daad533b7e36991a0515f970a2f963
Maksym Perehinets <supercomf128@gmail.com>
2025-06-22 23:06:16 +0300

    minor fix to make it ready for k8s and Dockerfile added

--- discord_ai_assistant/main.go
+++ discord_ai_assistant/main.go
@@ -4,29 +4,17 @@ import (
 	"context"
 	"github.com/Maksym-Perehinets/discord_ai_assistant/ai_client"
 	"github.com/Maksym-Perehinets/discord_ai_assistant/bot"
+	"os"
 )
 
 func main() {
+	AIAssistantAPIURL := os.Getenv("AI_ASSISTANT_API_URL")
 	b := bot.StartBot()
 	exitHandler := b.Run()
-	aiClient := ai_client.NewClient("http://localhost:8000", 600) // Initialize AI client with a timeout of 5000 milliseconds
+	aiClient := ai_client.NewClient(AIAssistantAPIURL, 600) // Initialize AI client with a timeout of 5000 milliseconds
 
 	commandHandlers := bot.AddCommandHandlerForQuery(context.TODO(), "what", aiClient.ProcessQuery)
 
-	//resp, err := aiClient.ProcessQuery(context.TODO(), ai_client.ChatRequest{
-	//	UserID:    "1234567890", // Example user ID
-	//	MessageID: "0987654321", // Example message ID
-	//	Query: []ai_client.ChatMessage{
-	//		{
-	//			Role:    "user",
-	//			Content: "discussion regarding tasty snacks",
-	//		},
-	//	},
-	//})
-
-	//if err != nil {
-	//	log.Fatal(err) // Handle error appropriately in production code
-	//}
 	b.SetUpSlashCommands("1350120716497846374", commands, commandHandlers)
 
 	exitHandler()
